a291613d355e49f546da6b2e45f56d654055a9bc
rnguyen89 <rnguyen89@gmail.com>
2021-12-04 00:24:17 -0800

    added feature to close modal by clicking on background

--- lightboxes/lightbox.js
+++ lightboxes/lightbox.js
@@ -1,17 +1,22 @@
 console.log("test")
 
+const bodyTag = document.querySelector("body")
 const photoTags = document.querySelectorAll("a img")
 modalCloseTag = document.querySelector(".modal-close")
+modalBackgroundTag = document.querySelector(".modal-background")
 modalContentTag = document.querySelector(".modal-content img")
 modalTag = document.querySelector(".modal")
 idImgTag = document.getElementById("modal-img")
 
+
 const closeModal = () => {
-  modalCloseTag.addEventListener("click", () => {
+  bodyTag.addEventListener("click", event => {
+    if (event.target !== modalCloseTag && event.target !== modalBackgroundTag) {
+      return
+    }
     modalTag.style.display = "none"
   })
 }
-
 
 photoTags.forEach((photoTag) => {
   console.log( "looped")
@@ -34,8 +39,5 @@ photoTags.forEach((photoTag) => {
 
   })
 })
-
-
-
 
 closeModal()
