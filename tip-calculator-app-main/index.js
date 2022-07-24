a79787d6981ba85ed574d69dcc9b06e4a4efea42
rnguyen89 <rnguyen89@gmail.com>
2022-07-24 15:17:04 -0700

    added fn to render input to results section

--- tip-calculator-app-main/index.js
+++ tip-calculator-app-main/index.js
@@ -2,6 +2,8 @@ const bill = document.getElementById("bill");
 const numOfPeople = document.getElementById("numOfPeople");
 const tipBtns = document.querySelectorAll(".tip-btn");
 const btnReset = document.getElementById("btn-reset");
+const totalTip = document.getElementById("total-tip");
+const totalPerson = document.getElementById("total-person");
 
 // tip tags
 const tipBtn5 = document.querySelector(".tip-btn-5");
@@ -10,3 +12,28 @@ const tipBtn15 = document.querySelector(".tip-btn-15");
 const tipBtn25 = document.querySelector(".tip-btn-25");
 const tipBtn50 = document.querySelector(".tip-btn-50");
 const tipBtnCustom = document.querySelector(".tip-btn-custom");
+
+// add click event to tip buttons
+// calculate tip % with total bill
+tipBtn5.addEventListener("click", function () {});
+tipBtn10.addEventListener("click", function () {});
+tipBtn15.addEventListener("click", function () {});
+tipBtn25.addEventListener("click", function () {});
+tipBtn50.addEventListener("click", function () {});
+tipBtnCustom.addEventListener("click", function () {});
+
+// get total bill
+
+// get total number of people
+
+// reset input values
+
+bill.addEventListener("keyup", function () {
+  totalTip.textContent = this.value * 0.05;
+});
+
+numOfPeople.addEventListener("keyup", function () {
+  let result =
+    (parseInt(bill.value) + parseInt(totalTip) * 0.05) / numOfPeople.value;
+  totalPerson.textContent = result;
+});
