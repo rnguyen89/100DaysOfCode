671519b7a13d2500efde4192e6853a6c08b9b268
rnguyen89 <rnguyen89@gmail.com>
2022-07-23 18:52:51 -0700

    added dom tags

---
+++ tip-calculator-app-main/index.js
@@ -0,0 +1,12 @@
+const bill = document.getElementById("bill");
+const numOfPeople = document.getElementById("numOfPeople");
+const tipBtns = document.querySelectorAll(".tip-btn");
+const btnReset = document.getElementById("btn-reset");
+
+// tip tags
+const tipBtn5 = document.querySelector(".tip-btn-5");
+const tipBtn10 = document.querySelector(".tip-btn-10");
+const tipBtn15 = document.querySelector(".tip-btn-15");
+const tipBtn25 = document.querySelector(".tip-btn-25");
+const tipBtn50 = document.querySelector(".tip-btn-50");
+const tipBtnCustom = document.querySelector(".tip-btn-custom");
