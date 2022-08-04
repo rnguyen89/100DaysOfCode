b677bc03b525dcfdcb030f418844b3579201de05
rnguyen89 <rnguyen89@gmail.com>
2022-08-04 16:16:21 -0700

    console.log computer choice

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,1 +1,19 @@
-console.log("hello world hihihihi");
+console.log("hello world!");
+
+const choice = ["rock", "paper", "scissor"];
+
+function playRound(playerSelection, computerSelection) {
+  // your code here!
+  console.log(playerSelection.toLowerCase() + " " + computerSelection);
+}
+
+function getComputerChoice() {
+  let selection = choice[Math.floor(Math.random() * choice.length)];
+  return selection;
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
