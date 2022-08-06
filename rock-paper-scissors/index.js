12f7cd6e01694c2415428e89687cc26bdefa827e
rnguyen89 <rnguyen89@gmail.com>
2022-08-05 23:57:48 -0700

    added conditional to log win and lose for player selection rock

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,19 +1,34 @@
 console.log("hello world!");
 
 const choice = ["rock", "paper", "scissor"];
+let selection = "";
 
 function playRound(playerSelection, computerSelection) {
   // your code here!
   console.log(playerSelection.toLowerCase() + " " + computerSelection);
+  if (playerSelection === computerSelection) console.log("TIE");
+  if (playerSelection === "rock" && computerSelection === "scissor")
+    console.log("YOU WIN!");
+  if (playerSelection === "rock" && computerSelection === "paper")
+    console.log("YOU LOSE!");
 }
 
 function getComputerChoice() {
-  let selection = choice[Math.floor(Math.random() * choice.length)];
+  selection = choice[Math.floor(Math.random() * choice.length)];
   return selection;
 }
 
-const playerSelection = prompt(`please type "rock", "paper", or "scissor"`);
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound(playerSelection, computerSelection);
+    console.log(computerSelection);
+  }
+}
+
+// const playerSelection = prompt(`please type "rock", "paper", or "scissor"`);
+const playerSelection = "rock";
 const computerSelection = getComputerChoice();
+game();
 
 console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
