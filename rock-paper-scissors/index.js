8a86f0942ee91dfe965998040f80288ed81d8c98
rnguyen89 <rnguyen89@gmail.com>
2022-08-06 01:08:17 -0700

    updated playRound fn to print wins and loses for all scenarios

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,34 +1,38 @@
 console.log("hello world!");
 
 const choice = ["rock", "paper", "scissor"];
-let selection = "";
+
+let i = 0;
 
 function playRound(playerSelection, computerSelection) {
   // your code here!
   console.log(playerSelection.toLowerCase() + " " + computerSelection);
-  if (playerSelection === computerSelection) console.log("TIE");
-  if (playerSelection === "rock" && computerSelection === "scissor")
-    console.log("YOU WIN!");
-  if (playerSelection === "rock" && computerSelection === "paper")
-    console.log("YOU LOSE!");
+  if (playerSelection === computerSelection) {
+    console.log("TIE");
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissor") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissor" && computerSelection === "paper")
+  ) {
+    console.log("You win");
+  } else console.log("YOU LOSE!");
 }
 
 function getComputerChoice() {
-  selection = choice[Math.floor(Math.random() * choice.length)];
+  let selection = choice[Math.floor(Math.random() * choice.length)];
   return selection;
 }
 
 function game() {
   for (let i = 0; i < 5; i++) {
     playRound(playerSelection, computerSelection);
-    console.log(computerSelection);
+    // prompt("type something");
   }
 }
 
-// const playerSelection = prompt(`please type "rock", "paper", or "scissor"`);
-const playerSelection = "rock";
+const playerSelection = prompt(`please type "rock", "paper", or "scissor"`);
+// const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 game();
 
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
