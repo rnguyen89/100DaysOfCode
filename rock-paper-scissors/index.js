ebf27d09cd177ef28b367b49785f9d4302ce0abf
rnguyen89 <rnguyen89@gmail.com>
2022-08-11 22:04:29 -0700

    fixed bug. for loop returns 5 random computer choices

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -4,50 +4,59 @@ const choice = ["rock", "paper", "scissor"];
 
 let i = 0;
 
+// function playRound(playerSelection, computerSelection) {
+//   // your code here!
+//   console.log(playerSelection.toLowerCase() + " " + computerSelection);
+//   if (playerSelection === computerSelection) {
+//     console.log("TIE!");
+//   } else if (
+//     (playerSelection === "rock" && computerSelection === "scissor") ||
+//     (playerSelection === "paper" && computerSelection === "rock") ||
+//     (playerSelection === "scissor" && computerSelection === "paper")
+//   ) {
+//     console.log(
+//       "You Win! " +
+//         playerSelection.charAt(0).toUpperCase() +
+//         playerSelection.slice(1) +
+//         " beats " +
+//         computerSelection.charAt(0).toUpperCase() +
+//         computerSelection.slice(1)
+//     );
+//   } else
+//     console.log(
+//       "You Lose! " +
+//         computerSelection.charAt(0).toUpperCase() +
+//         computerSelection.slice(1) +
+//         " beats " +
+//         playerSelection.charAt(0).toUpperCase() +
+//         playerSelection.slice(1)
+//     );
+// }
+
 function playRound(playerSelection, computerSelection) {
   // your code here!
-  console.log(playerSelection.toLowerCase() + " " + computerSelection);
-  if (playerSelection === computerSelection) {
-    console.log("TIE!");
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissor") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissor" && computerSelection === "paper")
-  ) {
-    console.log(
-      "You Win! " +
-        playerSelection.charAt(0).toUpperCase() +
-        playerSelection.slice(1) +
-        " beats " +
-        computerSelection.charAt(0).toUpperCase() +
-        computerSelection.slice(1)
-    );
-  } else
-    console.log(
-      "You Lose! " +
-        computerSelection.charAt(0).toUpperCase() +
-        computerSelection.slice(1) +
-        " beats " +
-        playerSelection.charAt(0).toUpperCase() +
-        playerSelection.slice(1)
-    );
+  console.log(playerSelection + " " + computerSelection);
 }
 
 function getComputerChoice() {
-  let selection = choice[Math.floor(Math.random() * choice.length)];
-  return selection;
+  return choice[Math.floor(Math.random() * choice.length)];
 }
 
 function game() {
   for (let i = 0; i < 5; i++) {
-    playRound(playerSelection, computerSelection);
-    // prompt("type something");
+    const computerSelection = getComputerChoice();
+
+    console.log(playRound(playerSelection, computerSelection));
   }
 }
 
-const playerSelection = prompt(`please type "rock", "paper", or "scissor"`);
-// const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-game();
+// const playerSelection = prompt(`please type "rock", "paper", or "scissor"`);
+const playerSelection = "rock";
+// game();
 
 // console.log(playRound(playerSelection, computerSelection));
+// for (let i = 0; i < 5; i++) {
+//   playRound(playerSelection, computerSelection);
+// }
+
+game();
