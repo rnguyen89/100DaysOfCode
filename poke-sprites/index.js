0737f8cca8dfff52c8709d40e88c0ab244844017
rnguyen89 <rnguyen89@gmail.com>
2022-09-23 22:59:10 -0700

    created html form

--- poke-sprites/index.js
+++ poke-sprites/index.js
@@ -1,18 +1,22 @@
 console.log("hello from index");
 
 const pokemonContainer = document.getElementById("pokemons");
+const pokeForm = document.getElementById("pokeform");
 
 let num = 150;
 const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${num}.png`;
 
 const pokemon144 = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/144.png`;
 
-// will append the first 150 pokemons to DOM
+// will append the first num pokemons to DOM
 // create function to loop 150 times
-// in loop, create img tag
-// set src to url
-// increment num to set pokemon
-// append to pokemon container
+// in loop, render pokemon
+// create render function
+//// render a div
+//// append img of pokemon and p of pokemon number
+//// set src to url
+//// increment num to set pokemon
+//// append to pokemon container
 
 function pokemonLoop(x, i) {
   for (let i = 1; i <= x; i++) {
@@ -40,4 +44,4 @@ function renderPokemon(url, i) {
 }
 
 // renderPokemon();
-pokemonLoop(251);
+pokemonLoop(3);
